4194996b3995443dc0664b39077ee675f57e11ae
johnbarness <johnotunba110@gmail.com>
2023-02-27 16:00:17 -0800

    task 4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,8 +6,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
-Prototype: void print_rev(char *s);
-void rev_string(char *s);
-void puts_half(char *str);
+void print_rev(char *s);
 
 #endif
